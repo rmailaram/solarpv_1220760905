c891412eb9db66a9a16ff2decf41d147b09f0d65
rakesh mailaram <rmailara@asu.edu>
2021-11-26 00:52:23 +0530

    final change

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Product, Certificate, Service
+from .serializers import ProductSerializer, CertificateSerializer, ServiceSerializer
 
-# Create your views here.
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class CertificateListView(generics.ListAPIView):
+    queryset = Certificate.objects.all()
+    serializer_class = CertificateSerializer
+
+
+class CertificateDetailView(generics.RetrieveAPIView):
+    queryset = Certificate.objects.all()
+    serializer_class = CertificateSerializer
+
+
+class ServiceListView(generics.ListAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
+
+class ServiceDetailView(generics.RetrieveAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
